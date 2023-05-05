3a3d23279a10669a5d37a53b4e70dcffa5ef1e65
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-05-05 13:53:10 +0200

    Last update Fr 5. Mai 13:53:10 CEST 2023

--- latex_to_pdf.sh
+++ latex_to_pdf.sh
@@ -1,13 +1,16 @@
 name_work="Dokumentation_COPRA-Biosignaldaten_in_FHIR"
-da=`date +%d-%m-%Y`
-date_sign="_digitalSigned_$da"
-if [ ! -f "$name_work.bcf" ]
-then 
+#da=`date +%d-%m-%Y`
+# date_sign="_digitalSigned_$da"
+#if [ ! -f "$name_work.bcf" ]
+#then 
+ # pdflatex $name_work.tex
+#  biber $name_work
+#  pdflatex --interaction=batchmode $name_work.tex 2>&1 /dev/null
+#else
+for i in 1 2 
+do
   pdflatex $name_work.tex
-  biber $name_work
-  pdflatex --interaction=batchmode $name_work.tex 2>&1 /dev/null
-else
-  pdflatex $name_work.tex
-fi
+done
+#fi
 
 #cp $name_work.pdf $name_work$date_sign.pdf
--- rm_no_importan.sh
+++ rm_no_importan.sh
@@ -1,1 +1,11 @@
-rm *.aux *.bbl *.log *.blg *.out *.toc
+name_work="Dokumentation_COPRA-Biosignaldaten_in_FHIR"
+rm $name_work.aux
+rm $name_work.log 
+rm $name_work.out
+rm $name_work.toc
+
+if [ -f "$name_work.synctex.gz" ]
+then 
+  rm $name_work.synctex.gz
+fi
+# rm *.syntex.gz
